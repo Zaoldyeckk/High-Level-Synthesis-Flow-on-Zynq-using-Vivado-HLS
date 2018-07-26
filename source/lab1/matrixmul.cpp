cb69211b8a1fdcedba21052ff3481e3703982f00
Zaoldyeckk <549136443@qq.com>
2018-07-26 21:36:36 +0800

    Create matrixmul.cpp

---
+++ source/lab1/matrixmul.cpp
@@ -0,0 +1,85 @@
+//*****************************************************************************
+// (c) Copyright 2009 - 2012 Xilinx, Inc. All rights reserved.
+//
+// This file contains confidential and proprietary information
+// of Xilinx, Inc. and is protected under U.S. and
+// international copyright and other intellectual property
+// laws.
+//
+// DISCLAIMER
+// This disclaimer is not a license and does not grant any
+// rights to the materials distributed herewith. Except as
+// otherwise provided in a valid license issued to you by
+// Xilinx, and to the maximum extent permitted by applicable
+// law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+// WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+// AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+// BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+// INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+// (2) Xilinx shall not be liable (whether in contract or tort,
+
+// liability) for any loss or damage of any kind or nature
+// related to, arising under or in connection with these
+// materials, including for any direct, or any indirect,
+// special, incidental, or consequential loss or damage
+// (including loss of data, profits, goodwill, or any type of
+// loss or damage suffered as a result of any action brought
+// by a third party) even if such damage or loss was
+// reasonably foreseeable or Xilinx had been advised of the
+// possibility of the same.
+//
+// CRITICAL APPLICATIONS
+// Xilinx products are not designed or intended to be fail-
+// safe, or for use in any application requiring fail-safe
+// performance, such as life-support or safety devices or
+// systems, Class III medical devices, nuclear facilities,
+// applications related to the deployment of airbags, or any
+// other applications that could lead to death, personal
+// injury, or severe property or environmental damage
+// (individually and collectively, "Critical
+// Applications"). Customer assumes the sole risk and
+// liability of any use of Xilinx products in Critical
+// Applications, subject only to applicable laws and
+// regulations governing limitations on product liability.
+//
+// THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+// PART OF THIS FILE AT ALL TIMES.
+//
+//*****************************************************************************
+//   ____  ____
+//  /   /\/   /
+// /___/  \  /    Vendor: Xilinx
+// \   \   \/     Version: %version
+//  \   \         Application: AutoESL
+//  /   /         Filename: matrixmul.cpp
+// /___/   /\     Date Last Modified: $Date: 2012/3/30 18:53:07 $
+// \   \  /  \    Date Created: Fri Mar 30 2012
+//  \___\/\___\
+//
+//Device: All
+//Design Name: matrixmul
+//Purpose:
+//    This is a C++ version of a matrix multiplier example.
+//Reference:
+//Revision History:
+//*****************************************************************************
+
+#include "matrixmul.h"
+
+void matrixmul(
+      mat_a_t a[MAT_A_ROWS][MAT_A_COLS],
+      mat_b_t b[MAT_B_ROWS][MAT_B_COLS],
+      result_t res[MAT_A_ROWS][MAT_B_COLS])
+{
+  // Iterate over the rows of the A matrix
+  Row: for(int i = 0; i < MAT_A_ROWS; i++) {
+    // Iterate over the columns of the B matrix
+    Col: for(int j = 0; j < MAT_B_COLS; j++) {
+      // Do the inner product of a row of A and col of B
+      res[i][j] = 0;
+      Product: for(int k = 0; k < MAT_B_ROWS; k++) {
+        res[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+}
